87315e621d3d24ecd9b022cca34ff8d00321fda2
pawaranand <pawaranand00@gmail.com>
2015-10-16 10:02:22 -0400

    new ch

---
+++ frappe/core/doctype/report/test_report.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2013, Web Notes Technologies Pvt. Ltd. and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+test_records = frappe.get_test_records('Report')
+
+class TestReport(unittest.TestCase):
+	pass
